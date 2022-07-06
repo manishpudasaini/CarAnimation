c9906d7c4dea1273d18bd7ed7e7c17cd5f611b8d
Manish Pudasaini <72080143+manishpudasaini@users.noreply.github.com>
2022-07-06 22:18:23 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function clickMe() {
+  let element = (document.getElementById("light").src = "assets/Img_06.png");
+}
+function clickMe1() {
+  let element = (document.getElementById("light").src = "assets/Img_05.png");
+}
